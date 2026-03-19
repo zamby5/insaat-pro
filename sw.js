7abb2c67c1916a0d710b5f43c9f6b0bfb3deff60
zamby5 <miyicioglu@gmail.com>
2026-03-19 14:04:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='insaat-pro-v4';
-const ASSETS=['./','./index.html'];
+const CACHE='insaat-pro-v5';
+const ASSETS=['./','./index.html','./klavuz.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
